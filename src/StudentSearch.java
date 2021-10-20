74a199f028a15a9f222b517ee14af408d9e2b540
Mustafa Yuşa Babademez <yusa.babademez@ug.bilkent.edu.tr>
2021-10-20 17:39:44 +0300

    Some comments added

--- src/StudentSearch.java
+++ src/StudentSearch.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-
+//some comments
 public class StudentSearch {
     public boolean studentExists(ArrayList<Student> students, String id) throws Exception {
         if (students.isEmpty()) {
