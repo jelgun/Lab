fada5ff69d82ae01651c5306754eb668897f032a
Ata Seren <ata.seren@ug.bilkent.edu.tr>
2021-10-20 18:11:27 +0300

    implement findOne method

--- src/StudentSearch.java
+++ src/StudentSearch.java
@@ -1,6 +1,13 @@
 import java.util.ArrayList;
 
 public class StudentSearch {
+    public Student findOne(ArrayList<Student> students, String name) throws Exception {
+for (Student student: students)
+if (student.getName().equals(name))
+return student;
+throw new Exception("There is no student with the given name!");
+}
+
     public boolean studentExists(ArrayList<Student> students, String id) throws Exception {
         if (students.isEmpty()) {
             throw new Exception("Students list should not be empty!");
